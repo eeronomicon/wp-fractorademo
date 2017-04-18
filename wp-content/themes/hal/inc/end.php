62e35226af775e011faa9c074d8af4e5c721d604
HK Kahng <hk@router-rooter.com>
2017-04-18 16:25:03 +0000

    make local commit for push to production

---
+++ wp-content/themes/hal/inc/end.php
@@ -0,0 +1,15 @@
+<footer id="end" class="clearfix">
+  <div class="col-md-4 col-lg-5 col-lg-offset-1">
+    <a href="/" class="end-logo"></a>
+    <?php bloginfo('name') ?> © <?php echo date('Y'); ?> All rights reserved. Website by <a href="https://www.inboundrx.com" target="_new">InboundRx</a>.
+  </div>
+
+  <div class="col-md-8 col-lg-5 col-lg-offset-1">
+    <ul class="nav nav-pills">
+      <li><a href="#intro">Home</a></li>
+      <li><a href="#intro-services">Services</a></li>
+      <li><a href="#faq">FAQs</a></li>
+      <li><a href="#locations">Contact</a></li>
+    </ul>
+  </div>
+</footer>
---
+++ wp-content/themes/hal/inc/head.php
@@ -0,0 +1,34 @@
+<header id="top" class="clearfix">
+
+  <div class="col-sm-3 col-md-2 col-md-offset-1">
+    
+  </div>
+  
+  <div class="col-sm-6 col-md-6 text-center">
+    <a href="/" class="top-logo"></a>
+  </div>
+
+  <div class="col-sm-3 col-md-2 col-md-offset-1">
+    <a href="#" id="top-burger"><span class="fa fa-bars"></span></a>
+  </div>
+
+  <div id="top-nav">
+    <div id="top-nav-close">
+      <a href="#"><span class="fa fa-close"></span></a>
+    </div>
+
+    <div id="top-nav-logo" class="visible-xs">
+      <a href="/" class="top-logo"></a>
+    </div>
+
+    <nav id="top-nav-menu">
+      <ul class="nav">
+      <li><a href="#intro">Home</a></li>
+      <li><a href="#intro-services">Services</a></li>
+      <li><a href="#faq">FAQs</a></li>
+      <li><a href="#locations">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+  
+</header>
